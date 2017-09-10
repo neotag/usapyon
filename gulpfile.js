d02f2b2aadd44c31a78433747928f4a15f4481b7
neotag <harada@gaji.jp>
2017-09-10 23:54:59 +0900

    Create task for building js

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,30 @@
 const gulp = require('gulp');
+const gutil = require('gulp-util');
+const source = require("vinyl-source-stream")
+const browserify = require('browserify');
+const babelify = require('babelify');
 const webserver = require('gulp-webserver');
 
 const HTDOCS = "docs/";
+const SRC_ROOT = "src/"
+const SRC_JS = SRC_ROOT + "js/";
+const JS_ENTRY_FILE = "app.js";
+const DIST_ROOT = HTDOCS + "assets/";
+const DIST_JS = DIST_ROOT + "js/";
+
+const js = () => {
+  browserify({
+    entries: SRC_JS + JS_ENTRY_FILE,
+    extensions: ['js']
+  })
+    .transform(babelify)
+    .bundle()
+    .on('error', gutil.log)
+    .pipe(source(JS_ENTRY_FILE))
+    .pipe(gulp.dest(DIST_JS));
+}
+
+gulp.task('js', js);
 
 gulp.task('server', () => {
   gulp.src(HTDOCS)
@@ -12,5 +35,7 @@ gulp.task('server', () => {
     }));
 });
 
-gulp.task('default', [ 'server' ]);
+gulp.task('build', [ 'js' ]);
+gulp.task('watch', [ 'watchJs' ]);
+gulp.task('default', [ 'build', 'watch', 'server' ]);
 
